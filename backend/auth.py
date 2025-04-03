62411ac13038795c496cb4758d113b6786b9cc84
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-04-03 23:03:18 +0300

    fixed auth

--- backend/auth.py
+++ backend/auth.py
@@ -1,87 +1,114 @@
 import functools
 
 import psycopg2
-from flask import Blueprint, request, jsonify, redirect, url_for, flash, render_template, session, g
+from flask import Blueprint, request, redirect, url_for, render_template, session, g, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from backend import SUPABASE_URL
+from backend.db import open_connection
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        data = request.json
-        username = data.get('username')
-        password = data.get('password')
-        error = None
 
-        if not username:
-            error = 'username is required'
-        elif not password:
-            error = 'password is required'
-
-        if error is not None:
-            try:
-                conn = psycopg2.connect(SUPABASE_URL)
-                cur = conn.cursor()
-
-                cur.execute('INSERT INTO user (username, password) VALUES (%s, %s)',
-                            (username, generate_password_hash(password)))
-
-                conn.commit()
-                cur.close()
-                conn.close()
-
-            except psycopg2.IntegrityError:
-                error = f'User {username} is already registered'
-            except Exception as e:
-                error = f'Exception: {str(e)}'
-            else:
-                return redirect(url_for('smth'))
-
-        flash(error)  # я хз як тут краще, бо якщо return jsonify, то помилка коли вже є зареєстрований юзер не пройде
-
-    return render_template('register.html')
+# @auth.route('/register', methods=('POST',))
+# def register():
+#     data = request.json
+#     username = data.get('username')
+#     password = data.get('password')
+#
+#     if not username:
+#         return jsonify({'error': 'username is required'}), 400
+#     elif not password:
+#         return jsonify({'error': 'password is required'}), 400
+#
+#     conn = open_connection()
+#     cur = conn.cursor()
+#     try:
+#         query = '''
+#             insert into "user" (username, password) values (%s, %s) returning user_id
+#             '''
+#         cur.execute(query, (username, generate_password_hash(password)))
+#         user_id = cur.fetchone()[0]
+#         conn.commit()
+#     except psycopg2.IntegrityError:
+#         return jsonify({'error': 'username already exists'}), 400
+#     finally:
+#         cur.close()
+#         conn.close()
+#     session['user_id'] = user_id
+#     return jsonify({'message': 'user created successfully'}), 201
 
 
 @auth.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
+        if g.user is not None:
+            return jsonify({'error': 'already logged in'}), 400
+
         data = request.json
         username = data.get('username')
         password = data.get('password')
-        error = None
 
         if not username:
-            error = 'username is required'
+            return jsonify({'error': 'username is required'}), 400
         elif not password:
-            error = 'password is required'
+            return jsonify({'error': 'password is required'}), 400
 
-        if error is not None:
-            try:
-                conn = psycopg2.connect(SUPABASE_URL)
-                cur = conn.cursor()
+        conn = open_connection()
+        cur = conn.cursor()
 
-                cur.execute('SELECT * FROM user WHERE username = %s', (username,))
-                user = cur.fetchone()
+        get_user_query = '''
+        select user_id, username, password from "user" where username = %s
+        '''
+        cur.execute(get_user_query, (username,))
+        user = cur.fetchone()
+        cur.close()
 
-                if user is None:
-                    error = 'there is no such a user'
-                elif not check_password_hash(user[2], password):
-                    error = 'incorrect password'
+        if user is None or not check_password_hash(user[2], password):
+            return jsonify({'error': 'incorrect username or password'}), 400
 
-                if error is None:
-                    session.clear()
-                    session['user_id'] = user[0]
-                    return redirect(url_for('index'))
+        cur = conn.cursor()
+        get_role_query = '''
+        select empl_role, empl_surname, empl_name, empl_patronymic from employee where id_employee = %s
+        '''
+        cur.execute(get_role_query, (user[0],))
+        employee = cur.fetchone()
 
-            except Exception as e:
-                error = f'Exception: {str(e)}'
+        cur.close()
+        conn.close()
 
-        flash(error)
+        session.clear()
+        session['user_id'] = user[0]
+        session['role'] = employee[0] if employee[0] else None
+        return jsonify({
+            'user_id': user[0],
+            'role': employee[0],
+            'empl_surname': employee[1],
+            'empl_name': employee[2],
+            'empl_patronymic': employee[3]}), 200
 
-    return render_template('login.html')
+    if request.method == 'GET':
+        if g.user is None:
+            return jsonify({'error': 'not logged in'}), 400
+
+        user_id = session.get('user_id')
+        conn = open_connection()
+        cur = conn.cursor()
+        get_role_query = '''
+                select empl_role, empl_surname, empl_name, empl_patronymic from employee where id_employee = %s
+                '''
+        cur.execute(get_role_query, (user_id,))
+        employee = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        return jsonify({
+            'user_id': user_id,
+            'role': employee[0],
+            'empl_surname': employee[1],
+            'empl_name': employee[2],
+            'empl_patronymic': employee[3]}), 200
 
 
 @auth.before_app_request
@@ -91,24 +118,29 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        conn = psycopg2.connect(SUPABASE_URL)
+        conn = open_connection()
         cur = conn.cursor()
 
-        cur.execute('SELECT * FROM user WHERE id = %s', (user_id,))
+        cur.execute('''
+        SELECT * FROM employee WHERE id_employee = %s
+        ''', (user_id,))
         g.user = cur.fetchone()
 
+        cur.close()
+        conn.close()
 
-@auth.route('/logout')
+
+@auth.route('/logout', methods=('GET',))
 def logout():
     session.clear()
-    return redirect(url_for('smth'))
+    return jsonify({'message': 'logged out successfully'}), 200
 
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('loginpage'))
+            return jsonify({'error': 'not logged in'}), 400
         return view(**kwargs)
 
     return wrapped_view
