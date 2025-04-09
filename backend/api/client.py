0dbb61ffd8a095f4cc88a9f064c9a8661b1f4e72
mchemrl <mcchemro@gmail.com>
2025-04-09 15:13:43 +0300

    demo delete, update, add client card

--- backend/api/client.py
+++ backend/api/client.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, jsonify, request
 
 from ..decorators import manager_required, cashier_required
-from ..services.client_service import edit_client, dump_client, create_client
+from ..services.client_service import edit_client, dump_client, create_client, fetch_client
 
 client = Blueprint('client', __name__)
 
-# @client.route('/', methods=('GET',))
-# def get_client():
-#     pass
+@client.route('/', methods=('GET',))
+def get_client():
+    pass
 
 @client.route('/', methods=('GET',))
 def get_clients():
@@ -16,14 +16,86 @@ def get_clients():
 @client.route('/', methods=('POST',))
 @cashier_required
 def add_client():
-    pass
+    data = request.json;
+    card_number = data.get('card_number')
+    cust_surname = data.get('cust_surname')
+    cust_name = data.get('cust_name')
+    cust_patronymic = data.get('cust_patronymic')
+    phone_number = data.get('phone_number')
+    city = data.get('city')
+    street = data.get('street')
+    zip_code = data.get('zip_code')
+    percent = int(data.get('percent'))
+
+    if not card_number or not cust_name or not cust_surname or not phone_number or not percent:
+        return jsonify({'error': 'missing required fields'}), 400
+
+    if percent <= 0 or percent > 100:
+        return jsonify({'error': 'invalid percent'}), 400
+
+    if len(phone_number) > 13 or not phone_number.startswith('+'):
+        return jsonify({'error': 'invalid phone number'}), 400
+
+    create_client(card_number, cust_surname, cust_name, cust_patronymic,
+                  phone_number, city, street, zip_code, percent);
+    return jsonify({'message': 'client added!'}), 200
+
 
 @client.route('/', methods=('PUT',))
 @cashier_required
 def update_client():
-    pass
+    card_number = request.args.get('card_number', type=str)
+    if not card_number:
+        return jsonify({'error': 'missing card_number'}), 400
+
+    client = fetch_client(card_number)
+    if not client:
+        return jsonify({'error': 'client not found'}), 404
+
+    data = request.json;
+    card_number = data.get('card_number')
+    cust_surname = data.get('cust_surname')
+    cust_name = data.get('cust_name')
+    cust_patronymic = data.get('cust_patronymic')
+    phone_number = data.get('phone_number')
+    city = data.get('city')
+    street = data.get('street')
+    zip_code = data.get('zip_code')
+    percent = int(data.get('percent'))
+
+    if not card_number or not cust_name or not cust_surname or not phone_number or not percent:
+        return jsonify({'error': 'missing required fields'}), 400
+
+    if percent <= 0 or percent > 100:
+        return jsonify({'error': 'invalid percent'}), 400
+
+    if len(phone_number) > 13 or not phone_number.startswith('+'):
+        return jsonify({'error': 'invalid phone number'}), 400
+
+    edit_client(card_number, cust_surname, cust_name, cust_patronymic,phone_number, city, street, zip_code, percent)
+
+    return jsonify({
+        'card_number': card_number,
+        'cust_surname': cust_surname,
+        'cust_name': cust_name,
+        'cust_patronymic': cust_patronymic,
+        'phone_number': phone_number,
+        'city': city,
+        'street': street,
+        'zip_code': zip_code,
+        'percent': percent
+    }), 200
 
 @client.route('/', methods=('DELETE',))
-@manager_required
+#@manager_required
 def delete_client():
-    pass
+    card_number = request.args.get('card_number')
+    if not card_number:
+        return jsonify({'error': 'missing card_number'}), 400
+
+    client = fetch_client(card_number)
+    if not client:
+        return jsonify({'error': 'client not found'}), 404
+
+    dump_client(card_number)
+    return jsonify({'message': 'client deleted!'}), 200
--- backend/services/client_service.py
+++ backend/services/client_service.py
@@ -1,16 +1,66 @@
 from backend.db import get_connection
 
-def fetch_client():
-    pass
+def fetch_client(card_number):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            query = 'select * from customer_card where card_number = %s'
+            cur.execute(query, (card_number,))
+            client = cur.fetchone()
+
+            if client:
+                return {
+                    'card_number': client[0],
+                    'cust_surname': client[1],
+                    'cust_name':client[2],
+                    'cust_patronymic': client[3],
+                    'phone_number': client[4],
+                    'city':client[5],
+                    'street': client[6],
+                    'zip_code': client[7],
+                    'percent': client[8],
+                }
+            else:
+                return None
 
 def fetch_clients():
     pass
 
-def create_client():
-    pass
+def create_client(card_number, cust_surname, cust_name, cust_patronymic,
+                    phone_number, city, street, zip_code, percent):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            query = """
+                    insert into customer_card (card_number, cust_surname, cust_name, cust_patronymic, 
+                    phone_number, city, street, zip_code, percent)
+                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """
+            cur.execute(query, (card_number, cust_surname, cust_name, cust_patronymic,
+                    phone_number, city, street, zip_code, percent))
+            conn.commit()
 
-def dump_client():
-    pass
+def dump_client(card_number):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            query = 'delete from customer_card where card_number = %s'
+            cur.execute(query, (card_number,))
+            conn.commit()
 
-def edit_client():
-    pass
+def edit_client(card_number, cust_surname, cust_name, cust_patronymic,
+                    phone_number, city, street, zip_code, percent):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            query = '''
+                update customer_card 
+                set cust_surname = %s,
+                    cust_name = %s,
+                    cust_patronymic = %s,
+                    phone_number = %s,
+                    city = %s,
+                    street = %s,
+                    zip_code = %s,
+                    percent = %s
+                where card_number = %s
+            '''
+            cur.execute(query, (cust_surname, cust_name, cust_patronymic,
+                    phone_number, city, street, zip_code, percent, card_number))
+            conn.commit()
