d34e82e2722df1cd38f71984da67f2418054d7f2
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-04-21 00:14:38 +0300

    api/sales fixed

--- backend/services/sales_service.py
+++ backend/services/sales_service.py
@@ -7,22 +7,22 @@ def fetch_sales():
     with get_connection() as conn:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
             query = '''
-                SELECT receipt_number, id_employee, card_number, print_date, sum_total, vat
-                FROM receipt
-            '''
+                SELECT upc, receipt_number, product_number, selling_price
+                FROM sale
+                '''
             cur.execute(query)
             sales = cur.fetchall()
             return sales
 
 
-def fetch_sale_by_id(sale_id):
+def fetch_sale_by_id(receipts_id):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
             query = '''
-                SELECT receipt_number, id_employee, card_number, print_date, sum_total, vat
-                FROM receipt
+                SELECT upc, receipt_number, product_number, selling_price
+                FROM sale
                 WHERE receipt_number = %s
-            '''
-            cur.execute(query, (sale_id,))
+                '''
+            cur.execute(query, (receipts_id,))
             sale = cur.fetchone()
             return sale
